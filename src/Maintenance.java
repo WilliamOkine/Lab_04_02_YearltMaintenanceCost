c55680493836a1d66c71d1534d0ba02ccea33111
WilliamOkine <okinewn@mail.uc.edu>
2023-06-05 12:56:59 -0400

    Initial commit

---
+++ src/Maintenance.java
@@ -0,0 +1,17 @@
+public class Maintenance {
+            public static void main(String[] args) {
+                double springCost = 500.0;
+                double summerCost = 800.0;
+                double autumnCost = 600.0;
+                double winterCost = 700.0;
+
+                double totalCost = springCost + summerCost + autumnCost + winterCost;
+
+                System.out.println("Spring maintenance cost: $" + springCost);
+                System.out.println("Summer maintenance cost: $" + summerCost);
+                System.out.println("Autumn maintenance cost: $" + autumnCost);
+                System.out.println("Winter maintenance cost: $" + winterCost);
+                System.out.println("-----------------------------");
+                System.out.println("Total yearly maintenance cost: $" + totalCost);
+            }
+        }
